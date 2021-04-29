d1f1c135f10fffc02f388226890fc2abe998cf7b
André Moura <andre.benedicto@etec.sp.gov.br>
2021-04-29 19:54:08 -0300

    definir corpo da cobrinha

--- script.js
+++ script.js
@@ -6,18 +6,40 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-
-criarBG()
-criarCobrinha()
+let direction = 'right'
 
 function criarBG() {
-    context.fillStyle = 'lightgreen'
+    context.fillStyle = 'lightblue'
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
 function criarCobrinha() {
     for (let i = 0; i < snake.length; i++) {
-        context.fillStyle = 'green'
+        context.fillStyle = 'blue'
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
 }
+
+function iniciarJogo() {
+    criarBG()
+    criarCobrinha()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if (direction == 'right') snakeX += box
+    if (direction == 'left') snakeX -= box
+    if (direction == 'up') snakeY -= box
+    if (direction == 'down') snakeY += box
+
+    snake.pop()
+
+    const newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+const jogo = setInterval(iniciarJogo, 100)
